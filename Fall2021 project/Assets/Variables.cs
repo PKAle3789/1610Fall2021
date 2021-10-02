7cbdfa002dc950f4e3f9d5dc800035a700481424
Alejandro Cardenas <10821747@my.uvu.edu>
2021-10-01 23:21:07 -0600

    Added Variables

---
+++ Fall2021 project/Assets/Variables.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Variables : MonoBehaviour
+{
+    string name = "Ale";
+    Console.WriteLine(name);
+
+    int myFive = 55;
+    myFive = 555; // myNum is now 20
+    Console.WriteLine(myNum);
+
+    long myFour = 444444444444L;
+    Console.WriteLine(myFour);
+
+    float myPi = 3.14F;
+    Console.WriteLine(myPi);
+
+    double myDec = 20.99D;
+    Console.WriteLine(myDec);
+
+    float f1 = 35e3F;
+    double d1 = 12E4D;
+    Console.WriteLine(f1);
+    Console.WriteLine(d1);
+    {
+        
+    }
+}
