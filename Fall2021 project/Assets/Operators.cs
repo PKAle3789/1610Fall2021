80f4966fbadd467748d2986970c9d706c554e5ef
Alejandro Cardenas <10821747@my.uvu.edu>
2021-10-01 23:26:18 -0600

    Added Operators

---
+++ Fall2021 project/Assets/Operators.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Operators : MonoBehaviour
+{
+    int sum1 = 44 + 44;        // 88 (44 + 44)
+    int sum2 = sum1 + 44;      // 122 (88 + 44)
+    int sum3 = sum2 + sum2;     // 244 (122 + 122)
+
+    int x = 10;
+    x += 5;
+
+    x == y
+    x > y
+    x < y
+    x != y
+    x <= y
+    x >= y
+    {
+        
+    }
+}
