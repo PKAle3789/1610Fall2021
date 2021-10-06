96ec6b9bfc3e8adea0211a7f7a76c22b5a926dd0
Alejandro Cardenas <10821747@my.uvu.edu>
2021-10-05 22:32:47 -0600

    Added While Loops

---
+++ Fall2021 project/Assets/WhileLoops.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WhileLoops : MonoBehaviour
+{
+    int i = -4;
+    while (i< 45) 
+    {
+        Console.WriteLine(i);
+        i+2;
+    }
+}
