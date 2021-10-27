076878af845eee14ad115cb9ab99a114e9acc777
Alejandro Cardenas <10821747@my.uvu.edu>
2021-10-26 23:10:37 -0600

    Added Challenge 2
    play fetch

---
+++ Create With Code Ale C/Prototype 2/Assets/Challenge 2/Scripts/PlayerControllerX.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControllerX : MonoBehaviour
+{
+    // prevents player from spamming dogs
+    private float shotFrequency = 1;
+    private float nextShot = 0;
+    public GameObject dogPrefab;
+
+    // Update is called once per frame
+    void Update()
+    {
+        // On spacebar press, send dog when enough time has passed
+        if (Input.GetKeyDown(KeyCode.Space) && Time.time > nextShot)
+        {
+            nextShot = Time.time + shotFrequency; // resets next shot
+            Instantiate(dogPrefab, transform.position, dogPrefab.transform.rotation);
+        }
+    }
+}
