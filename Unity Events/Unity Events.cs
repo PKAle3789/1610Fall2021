60e28ad5161b3b2f6b8b7311728f339a195ce237
Alejandro Cardenas <10821747@my.uvu.edu>
2021-12-02 23:12:39 -0700

    Added Unity Events

---
+++ Unity Events/Unity Events.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class UnityEvents : MonoBehaviour
+{
+    // Practice
+    public UnityEvent CalvinAndHobbesEvent, BonesEvent, GarfieldEvent;
+
+    private void Calvin()
+    {
+        CalvinAndHobbesEvent.Invoke();
+    }
+
+    private void Bones()
+    {
+        BonesEvent.invoke();
+    }
+
+    private void Lasagna()
+    {
+        GarfieldEvent.Invoke();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
